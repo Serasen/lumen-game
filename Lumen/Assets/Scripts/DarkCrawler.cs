cc9f508cb5a45c8ca11f86de17d7c2778d60fd56
justin.keville@gmail.com <justin.keville@gmail.com@1012842c-5ef7-80b5-a8ab-41d05672d5ca>
2013-03-06 10:27:22 +0000

    Dark Crawler is prettier, but needs a better shader.

--- Lumen/Assets/Scripts/DarkCrawler.cs
+++ Lumen/Assets/Scripts/DarkCrawler.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class DarkCrawler : MonoBehaviour {
 	private int direction = 0;
-	private int speed = 10;
+	private int speed = 10, fallspeed = 10;
 	private bool midair = true;
 	private int waypoint = -1;
 	private PathFinder pf;
@@ -18,29 +18,39 @@ public class DarkCrawler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(pf.GetIloAt() > waypoint)
-			direction = 1;
-		else if(pf.GetIloAt() < waypoint)
 			direction = -1;
+		else if(pf.GetIloAt() < waypoint)
+			direction = 1;
 		if(!midair)
 			rigidbody.velocity = direction*speed*transform.right;
+		else {
+			rigidbody.velocity = transform.up*fallspeed;
+		}
+
+	}
+	
+	void FixedUpdate() {
+		
 	}
 	
 	void OnCollisionEnter(Collision collision) {
-		if(collision.gameObject.tag.Equals("Player"))
+		if(collision.gameObject.tag.Equals("Player")) {
+			waypoint = -1;
 			LevelManager.instance.getCurrentLevel().getCurrentRoom().reEnterRoom();
+		}
 	}
 	
 	void OnCollisionStay(Collision collision) {
 		transform.rotation = Quaternion.FromToRotation(Vector3.up, collision.contacts[0].normal);
 		Vector3 eulerAngles = transform.rotation.eulerAngles;
-    	eulerAngles = new Vector3(0, 0, eulerAngles.z);
+    	eulerAngles = new Vector3(0, 0, eulerAngles.z+180);
     	transform.rotation = Quaternion.Euler(eulerAngles);
 		midair = false;
 	}
 	
 	void OnCollisionExit(Collision collision) {
 		midair = true;
-		rigidbody.velocity = -transform.up*5;
+		rigidbody.velocity = transform.up*fallspeed;
 	}
 	
 	void OnCollisionStay() {
@@ -51,9 +61,9 @@ public class DarkCrawler : MonoBehaviour {
 		if(!t.Equals(lastWaypoint)) {
 			if(waypoint == -1)
 				waypoint = pf.GetWaypoints().IndexOf(t);
-			else if(direction == 1)
-				waypoint++;
 			else if(direction == -1)
+				waypoint++;
+			else if(direction == 1)
 				waypoint--;
 			lastWaypoint = t;
 		}
