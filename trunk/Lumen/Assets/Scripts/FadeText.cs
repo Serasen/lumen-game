9a6e3f07ca3de891281a70464c7265d79b83d8b3
bassist0403@yahoo.com <bassist0403@yahoo.com@1012842c-5ef7-80b5-a8ab-41d05672d5ca>
2013-03-14 19:24:10 +0000

    Fade Text works with any resolution, IloController refactored

--- trunk/Lumen/Assets/Scripts/FadeText.cs
+++ trunk/Lumen/Assets/Scripts/FadeText.cs
@@ -12,10 +12,12 @@ public class FadeText : MonoBehaviour {
 	[System.Serializable]
 	public class GUIInfo {
 		public string text;
-		public int fontSize;
-		public int posX;
-		public int posY;
+		public float fontSize;
+		public float posX;
+		public float posY;
 	}
+	
+	float pixelRatio;
 	
 	void Start() {
 		mainCamera = Game.instance.levelManager.getCamera();
@@ -37,16 +39,19 @@ public class FadeText : MonoBehaviour {
 	}
 	
 	void OnGUI() {
+		pixelRatio = (mainCamera.orthographicSize * 2) / mainCamera.pixelHeight;
+		
 		GUIStyle newStyle = new GUIStyle();
 		newStyle.alignment = TextAnchor.UpperCenter;
 		newStyle.normal.textColor = new Color(1,1,1,alphaValue);
-		Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.position);
 		Rect guiRect;
+		
 		foreach(GUIInfo elem in guiInfos) {
-			newStyle.fontSize = elem.fontSize;
-			guiRect = new Rect(screenPoint.x + elem.posX, Screen.height - (screenPoint.y + elem.posY), 0, 0);
+			newStyle.fontSize = (int) (elem.fontSize/pixelRatio);
+			Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.position + new Vector3(elem.posX, elem.posY, 0));
+			guiRect = new Rect(screenPoint.x, Screen.height - (screenPoint.y), 0, 0);
 			GUI.Box(guiRect, elem.text, newStyle);
-		}	
+		}
 	}
 	
 	IEnumerator fadeInText() {
