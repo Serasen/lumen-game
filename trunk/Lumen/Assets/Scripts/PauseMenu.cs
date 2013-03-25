211f6f7ffe32a0a4b31b2946e7d9129abaaa26f8
bassist0403@yahoo.com <bassist0403@yahoo.com@1012842c-5ef7-80b5-a8ab-41d05672d5ca>
2013-03-25 20:31:34 +0000

    Added pause menu

---
+++ trunk/Lumen/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public GUIInfo[] guiInfos;
+	public GUIInfo[] options;
+	
+	Color defaultColor = Color.white;
+	Color selectedColor = Color.gray;
+	int optionNum;
+	bool inputPressed;
+	
+	void OnEnabled() {
+		inputPressed = false;
+		optionNum = 0;
+	}
+	void OnGUI() {
+		GUIStyle newStyle = new GUIStyle();
+		newStyle.alignment = TextAnchor.MiddleCenter;
+		newStyle.normal.textColor = defaultColor;
+		Rect guiRect;
+		foreach(GUIInfo info in guiInfos) {
+			newStyle.fontSize = (int)info.fontSize;
+			Vector2 pos = new Vector2(info.posX*Screen.width/100f, (100-info.posY)*Screen.height/100f);
+			guiRect = new Rect(pos.x, pos.y, 0,0);
+			GUI.Box(guiRect, info.text, newStyle);
+		}
+		for(int i = 0; i < options.Length; i++) {
+			GUIInfo info = options[i];
+			newStyle.fontSize = (int)info.fontSize;
+			Vector2 pos = new Vector2(info.posX*Screen.width/100f, (100-info.posY)*Screen.height/100f);
+			guiRect = new Rect(pos.x, pos.y, 0,0);
+			newStyle.normal.textColor = (optionNum != i) ? defaultColor : selectedColor;
+			GUI.Box(guiRect, info.text, newStyle);
+		}
+		
+		HandleInput();
+	}
+	
+	void HandleInput() {
+		if(Input.GetKeyDown(KeyCode.DownArrow)) {
+			if(optionNum < options.Length - 1) optionNum++;
+		}
+		else if(Input.GetKeyDown(KeyCode.UpArrow)) {
+			if(optionNum > 0) optionNum--;
+		}
+		if(Input.GetKeyDown(KeyCode.Return)) {
+			switch(optionNum) {
+			case 0: //Unpause game
+				this.enabled = false;
+				Game.instance.Unpause();
+				break;
+			case 1:
+				Application.Quit();
+				break;
+			}
+		}		
+	}
+}
