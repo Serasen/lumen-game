daf70e17668bd251e14c2180d022421130211829
bassist0403@yahoo.com <bassist0403@yahoo.com@1012842c-5ef7-80b5-a8ab-41d05672d5ca>
2013-04-14 20:39:58 +0000

    Level stats now cycle through.

--- trunk/Lumen/Assets/Scripts/Level-Specific/Level Hub/Hub Room/LevelDisplayText.cs
+++ trunk/Lumen/Assets/Scripts/Level-Specific/Level Hub/Hub Room/LevelDisplayText.cs
@@ -3,42 +3,10 @@ using System.Collections;
 
 public class LevelDisplayText : FadeText {
 	int levelToDisplay;
-	LevelData levelData;
-	
 	
 	protected override void OnEnable() {
 		levelToDisplay = transform.parent.GetComponent<EnterLevelKeyhole>().levelToEnter;
-		levelData = Game.instance.dataManager.GetLevelData(levelToDisplay);
 		alphaValue = 0f;
 		guiInfos[0].text = "Level " + levelToDisplay;
-		if(guiInfos.Length > 2 && levelData != null && levelData.rooms != null) {
-			guiInfos[1].text = GetRoomProgress();
-			guiInfos[2].text = "Friends Saved: " + levelData.GetNumSavedFriends();
-		}
-		else {
-			guiInfos[1].text = "";	
-			guiInfos[2].text = "";
-		}
-	}
-	
-	string GetRoomProgress() {
-		string progressString = "";
-		if(levelData != null) {
-			int reachedRooms = levelData.GetNumReachedRooms();
-			int totalRooms = levelData.rooms.Length;
-			if(reachedRooms < totalRooms) {
-				progressString = "Rooms: " + reachedRooms + " of " + totalRooms;
-			}
-			else {
-				int reachedKeyholes = levelData.getNumReachedKeyholes();
-				int totalKeyholes = levelData.getNumKeyholes();
-				if(reachedKeyholes < totalKeyholes) {
-					progressString = "Keyholes Reached: " + reachedKeyholes + " of " + totalKeyholes;	
-				}
-				else progressString = "Completed!";
-			}
-		}
-		return progressString;
-	}
-	
+	}	
 }
