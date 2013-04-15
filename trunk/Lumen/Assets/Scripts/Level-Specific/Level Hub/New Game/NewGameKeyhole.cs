fef563835a20db4e9b076bc1e8f1e8b46f6e98ba
bassist0403@yahoo.com <bassist0403@yahoo.com@1012842c-5ef7-80b5-a8ab-41d05672d5ca>
2013-04-15 23:50:33 +0000

    Go to Level 1 if new game, instead of going to level menu.

--- trunk/Lumen/Assets/Scripts/Level-Specific/Level Hub/New Game/NewGameKeyhole.cs
+++ trunk/Lumen/Assets/Scripts/Level-Specific/Level Hub/New Game/NewGameKeyhole.cs
@@ -5,12 +5,13 @@ public class NewGameKeyhole : Keyhole {
 	public int saveFile;
 	
 	protected override void OnTriggerEnter(Collider collider) {
+		int levelToEnter = 1;
 		if(collider.tag == "Player") {
 			Game.instance.dataManager.StartNewGame(saveFile);
 			Game.instance.levelManager.initializeGameData();
 			Game.instance.dataManager.ChangeLevel(0);
 			Game.instance.levelManager.getCurrentLevel().initializeLevelData();
-			myRoom.keyholeReached(keyholeNum);
+			Game.instance.RoomTransition((int)LevelActions.CHANGE_LEVEL, levelToEnter);
 		}
 	}
 	
